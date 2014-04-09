b3e909291541f5035720fbca9478fa58f1999ea1
Pratik Pingale <pratik@onmego.com>
2014-04-09 07:38:50 -0400

    Add user following

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -35,6 +35,34 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
+      describe "in the Users controller" do
+
+        describe "visiting the edit page" do
+          before { visit edit_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch user_path(user) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the following page" do
+          before { visit following_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the followers page" do
+          before { visit followers_user_path(user) }
+          it { should have_title('Sign in') }
+        end
+      end
+
        describe "in the Microposts controller" do
 
         describe "submitting to the create action" do
@@ -64,23 +92,19 @@ describe "Authentication" do
         end
       end
 
-      describe "in the Users controller" do
-
-        describe "visiting the edit page" do
-          before { visit edit_user_path(user) }
-          it { should have_title('Sign in') }
-        end
-
-        describe "submitting to the update action" do
-          before { patch user_path(user) }
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
           specify { expect(response).to redirect_to(signin_path) }
         end
 
-        describe "visiting the user index" do
-          before { visit users_path }
-          it { should have_title('Sign in') }
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
+
+      
     end
 
     describe "as wrong user" do
